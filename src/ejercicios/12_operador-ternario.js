5b1f60433c7345386f48041cce0009173f60b835
mdCarron <mdcarron.dev@gmail.com>
2020-09-25 11:11:36 -0300

    Completa 16. Operador condicional ternario

---
+++ src/ejercicios/12_operador-ternario.js
@@ -0,0 +1,15 @@
+const activo = true;
+/* 
+let mensaje = "";
+
+if (activo) {
+  mensaje = "Usuario Activo";
+} else {
+  mensaje = "Usuario no Logueado";
+}
+ */
+
+// const mensaje = activo ? "Usuario Activo" : "Usuario No Logueado";
+const mensaje = activo && "Usuario Activo";
+
+console.log(mensaje);
